d7601b70988f1ebc5033923bd69119ea38410438
ibradevk-19 <eng.ibrahimabuismail@gmail.com>
2025-08-25 11:44:24 +0300

    df

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => 'password'
+            'email' => 'st-admin@ksach.org',
+            'password' => 'ksachAdmin@2009'
         ]);
     }
 }
